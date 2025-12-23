67fe608118dad4d34dcae3b454608fd411a81590
Jongisapho Ndeya <u22625926@tuks.co.za>
2025-12-23 13:14:39 +0200

    Initial frontend setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,16 @@ function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="min-h-screen bg-background text-foreground">
-        <h1 className='text-4l font-bold text-center pt-10'>
-          Welcome to My vite + React + shacn/ui app!
-        </h1>
+        {/* Responsive container with padding that grows on larger screens */}
+        <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+          {/* Pages, header, main content, etc. */}
+          <main>
+            {/*Starter content */}
+            <h1 className='text-3xl font-bold text-center py-10'>
+              My Responsive App
+            </h1>
+          </main>
+        </div>
       </div>
     </ThemeProvider>
   )
