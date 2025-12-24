c9712e6def528fccd31a54fe15b9860f03121c9e
Jongisapho Ndeya <u22625926@tuks.co.za>
2025-12-24 12:31:23 +0200

    Fixing Get Started button

--- frontend/src/components/shared/BtnLink.tsx
+++ frontend/src/components/shared/BtnLink.tsx
@@ -3,7 +3,8 @@ import type { BtnLinkProps } from "../interface/BtnLinkProps";
 
 export const BtnLink = ({href, text, className} : BtnLinkProps) => {
     return (
-        <a href={href} className={`px-6 py-3 rounded-full outline-none relative overflow-hidden border bg-blue-600 dark:bg-violet-600 cursor-pointer hover:scale-[1.1] ${className}`}>
+        <a href={href} className={`px-6 py-3 rounded-full outline-none relative overflow-hidden border bg-blue-600 dark:bg-violet-600 cursor-pointer 
+            transition-all duration-300 ease-out hover:scale-[1.05] ${className}`}>
             <span className="relative z-10 text-white"> {text} </span>
         </a>
     )
--- frontend/src/components/shared/NavItem.tsx
+++ frontend/src/components/shared/NavItem.tsx
@@ -3,7 +3,7 @@ import type { NavItemProps } from "../interface/NavItemProps"
 export const NavItem = ({ href, text = " " }: NavItemProps) => {
   return (
     <li>
-        <a href={href} className="duration-300 font-bold  hover:text-blue-500 py-3 px-5">
+        <a href={href} className="duration-300 font-bold ease-linear hover:text-blue-500 py-3 px-5">
             {text}
         </a>
     </li>
