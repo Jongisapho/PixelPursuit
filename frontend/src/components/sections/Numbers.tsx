37a7227b7043a28f3fe417e97bea84c6b601ae5d
Jongisapho Ndeya <u22625926@tuks.co.za>
2025-12-26 10:55:21 +0200

    Enhancing the numbers

--- frontend/src/components/sections/Numbers.tsx
+++ frontend/src/components/sections/Numbers.tsx
@@ -5,22 +5,22 @@ export const Numbers = () => {
         <Container className="flex justify-center align-center">
             <div className="mx-auto lg:mx-0 p-5 sm:p-6 sm:py-8 max-w-5xl rounded-3xl bg-box-bg
                             border border-box-border shadow-lg shadow-box-shadow md:divide-x-2 divide-box-border
-                            grid grid-cols-2 md:grid-cols-4">
+                            grid grid-cols-2 md:grid-cols-4 hover:border-blue-500  hover:border-2 ease-in-out transition-linear duration-600">
                 <div className="text-center px-5">
-                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">  100+ </h2>
+                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1"> 100<span className="text-blue-500">+</span> </h2>
                     <p className="mt-2 text-heading-3 "> Jobs created and linked with job seekers </p>
                 </div>
                 <div className="text-center px-5">
-                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">  200+ </h2>
-                    <p className="mt-2 text-heading-3 "> Applicants registered users in the application </p>
+                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">  200<span className="text-blue-500">+</span> </h2>
+                    <p className="mt-2 text-heading-3 "> Registered users in our website </p>
                 </div>
                 <div className="text-center px-5">
-                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">  400+ </h2>
+                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">  400<span className="text-blue-500">+</span> </h2>
                     <p className="mt-2 text-heading-3 "> Jobs available and open in the app </p>
                 </div>
                 <div className="text-center px-5">
-                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">  100+ </h2>
-                    <p className="mt-2 text-heading-3 "> Jobs created and linked with job seekers </p>
+                    <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl text-heading-1">  96.98<span className="text-blue-500">%</span>  </h2>
+                    <p className="mt-2 text-heading-3 "> Uptime guarantee</p>
                 </div>
             </div>
         </Container>
